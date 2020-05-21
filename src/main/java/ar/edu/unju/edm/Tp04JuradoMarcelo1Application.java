defb055d4a06f3d53e9351f158d9f6e48e001329
Marcelo <Marcelo@DESKTOP-DMV6N5B>
2020-05-21 15:02:02 -0300

    Inicializacion de Proyecto

---
+++ src/main/java/ar/edu/unju/edm/Tp04JuradoMarcelo1Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp04JuradoMarcelo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp04JuradoMarcelo1Application.class, args);
+	}
+
+}
